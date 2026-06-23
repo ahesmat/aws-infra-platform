2d3b4384681da59581f09f0b613ddf7351e7a1df
Ahmed Esmat <your-email@example.com>
2026-06-23 02:16:43 +0000

    feat: complete ALB, HTTPS listener, target group, Route53 alias record

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-aws-infra-platform-992382552000"
+    bucket       = "tfstate-aws-infra-platform-644123626050"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
