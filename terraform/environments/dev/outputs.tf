d251f59a184cec6f90824595af21fedb91e3ed89
ahesmat <ahmed.moh.esmat@gmail.com>
2026-06-27 20:58:21 -0700

    feat: add Terraform outputs for instance ID, ALB DNS, site URL, and nameservers

---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "The EC2 instance ID (used for SSM connections)"
+  value       = aws_instance.main.id
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS Name"
+  value       = aws_lb.main.dns_name
+}
+
+output "site_url" {
+  description = "Live HTTPS URL for the deployed site"
+  value       = "https://devops.reachlyapp.com"
+}
+
+output "route53_name_servers" {
+  description = "Route 53 Name Servers"
+  value       = aws_route53_zone.main.name_servers
+}
