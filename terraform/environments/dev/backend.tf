541bbc9fe529c9afcc1c572b6511a80e9717ad69
ahesmat <ahmed.moh.esmat@gmail.com>
2026-07-08 16:49:13 -0700

    feat: add Terraform CI workflow with fmt, validate, and tfsec

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-aws-infra-platform-348737449144"
+    bucket       = "tfstate-aws-infra-platform-058264166251"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
