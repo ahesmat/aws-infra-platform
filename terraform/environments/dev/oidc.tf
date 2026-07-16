a0063677e03b733587e2b0469f1b73056966024c
ahesmat <ahmed.moh.esmat@gmail.com>
2026-07-15 19:30:00 -0700

    fix: add sns permissions to GitHub Actions IAM role

--- terraform/environments/dev/oidc.tf
+++ terraform/environments/dev/oidc.tf
@@ -52,7 +52,8 @@ data "aws_iam_policy_document" "github_actions_document" {
       "acm:*",
       "ssm:*",
       "cloudwatch:*",
-      "logs:*"
+      "logs:*",
+      "sns:*"
     ]
 
     resources = ["*"]
