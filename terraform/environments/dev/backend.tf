fb16e055879fb537921b4fd43b83539e08ffb35e
ahesmat <ahmed.moh.esmat@gmail.com>
2026-07-15 17:49:26 -0700

    feat: add CloudWatch log groups and CloudWatch agent deployment via Ansible

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-aws-infra-platform-370445361629"
+    bucket       = "tfstate-aws-infra-platform-211125717379"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
