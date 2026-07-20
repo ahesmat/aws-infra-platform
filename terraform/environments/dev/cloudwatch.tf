d60b94bd0f92c56fdbe82fe3f9f68c35a7ff25cb
ahesmat <ahmed.moh.esmat@gmail.com>
2026-07-20 16:39:24 -0700

    fix: terraform fmt formatting fixes

--- terraform/environments/dev/cloudwatch.tf
+++ terraform/environments/dev/cloudwatch.tf
@@ -22,7 +22,7 @@ variable "alert_email" {
 
 resource "aws_sns_topic" "alerts" {
   kms_master_key_id = "alias/aws/sns"
-  name = "${var.project}-${var.environment}-alerts"
+  name              = "${var.project}-${var.environment}-alerts"
   tags = {
     Name = "${var.project}-${var.environment}-alerts"
   }
